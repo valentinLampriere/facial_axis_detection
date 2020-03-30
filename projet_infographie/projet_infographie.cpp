67ef5391c370e3cebc2ae3d93c474bc7078c9fc0
valentinLampriere <valentin.lampriere1@gmail.com>
2020-03-30 18:42:07 +0200

    Setup opencv + open file image

--- projet_infographie/projet_infographie.cpp
+++ projet_infographie/projet_infographie.cpp
@@ -1,20 +1,28 @@
-// projet_infographie.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <string>
+#include <opencv2/imgproc/imgproc.hpp>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace cv;
+using namespace std;
+
+
+
+int main(void) {
+
+	string path("../images/image_crop.jpg");
+
+	Mat img = imread(path);
+
+	if(img.empty()) {
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+
+	imshow("Image crop", img);
+
+	waitKey(0);
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
