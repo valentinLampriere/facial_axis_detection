1288e4dbf6608d8b0aa107552847929a0c00cfa4
vclimpont <46527505+vclimpont@users.noreply.github.com>
2020-03-30 20:24:43 +0200

    create bar chart of gray intensity values in the image

--- projet_infographie/projet_infographie.cpp
+++ projet_infographie/projet_infographie.cpp
@@ -21,8 +21,39 @@ int main(void) {
 		return -1;
 	}
 
-	imshow("Image crop", img);
+    vector<Mat> bgr_planes;
+    split(img, bgr_planes);
 
-	waitKey(0);
-	return 0;
+    int histSize = 256;
+
+    float range[] = { 0, 256 }; //the upper boundary is exclusive
+    const float* histRange = { range };
+
+    bool uniform = true, accumulate = false;
+
+    Mat g_hist;
+    calcHist(&bgr_planes[0], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
+
+    int hist_w = 512, hist_h = 400;
+    int bin_w = cvRound((double)hist_w / histSize);
+    Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(255, 255, 255));
+
+    normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
+
+    for (int i = 1; i < histSize; i++)
+    {
+        /*line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
+            Point(bin_w * (i), hist_h - cvRound(g_hist.at<float>(i))),
+            Scalar(255, 0, 0), 2, 8, 0);*/
+        line(histImage, Point(bin_w * (i), hist_h),
+            Point(bin_w * (i), hist_h - cvRound(g_hist.at<float>(i))),
+            Scalar(255, 0, 50), 2, 8, 0);    
+    }
+
+    imshow("Source image", img);
+    imshow("calcHist Demo", histImage);
+
+    waitKey(0);
+
+    return 0;
 }
